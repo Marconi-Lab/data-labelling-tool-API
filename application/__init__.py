3c00718103508e65359fe56d3f7c763527a0b43b
ben-wycliff <ben12wycliff@gmail>
2021-01-26 16:10:40 +0300

    Creates dataset post endpoint

--- application/__init__.py
+++ application/__init__.py
@@ -6,6 +6,8 @@ from instance.config import app_config
 from dotenv import load_dotenv
 load_dotenv()
 
+from flask import request, jsonify, abort
+
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -15,4 +17,21 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    @app.route('/datasets/', methods=["POST", "GET"])
+    def bucketlists():
+        if request.method == "POST":
+            data = request.data
+            if data.name and data.classes:
+                dataset = Dataset(name=data.name, classes=data.classes)
+                dataset.save()
+                response = jsonify({
+                  "id": dataset.id,
+                  "name": dataset.name,
+                  "classes": dataset.classes
+                  "date_created": dataset.date_created,
+                  "date_modified": dataset.date_modified  
+                })
+                resoponse.status_code = 201
+                return response
+        else:
     return app
