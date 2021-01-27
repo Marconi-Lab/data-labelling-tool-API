1727895f005e6c74c83a95158b3f40a0ef02be58
ben-wycliff <ben12wycliff@gmail.com>
2021-01-27 11:16:43 +0300

    Creates tests execution command

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 import os
+import unittest
+
 from flask_script import Manager # class for handling a set of commands
 from flask_migrate import Migrate, MigrateCommand
 from application import db, create_app
@@ -13,6 +15,15 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+# define  command for testing
+@manager.command
+def test():
+    """Runs the unit tests without test coverage"""
+    tests = unittest.TestLoader().discover('./tests', pattern="test*.py")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
