55f807ebe9e33a179eb75210ff06fba91f8f2d08
ben-wycliff <ben12wycliff@gmail>
2021-01-27 11:09:49 +0300

    Adds user model

--- application/models.py
+++ application/models.py
@@ -1,5 +1,6 @@
 from application import db
-
+from flask_user import UserMixin
+from flask_bcrypt import Bcrypt
 
 class Datasets(db.Model):
     """This class represents the datasets table"""
@@ -31,3 +32,45 @@ class Datasets(db.Model):
 
     def __repr__(self):
         return "<Dataset: {}>".format(self.name)
+
+class User(db.Model, UserMixin):
+    """This class defines the user table"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    # Define the relationship to Role via UserRoles
+    roles = db.relationship('Role', secondary='user_roles')
+
+    def __init__(self, email, password, username):
+        """Initialize the user with an email, username and a password"""
+        self.email = email
+        self.password = Bcrypt().generate_password_hash(password).decode()
+        self.username = username
+
+    def password_is_valid(self, password):
+        """Checks password against it's hash"""
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def save(self):
+        """Save a user to the database"""
+        db.session.add(self)
+        db.session.commit()
+
+# Define the Role data-model
+class Role(db.Model):
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
