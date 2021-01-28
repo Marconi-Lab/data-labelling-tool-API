20bda26ef45ea5d018f5ba96c1c46fa50e5b0fb2
ben-wycliff <ben12wycliff@gmail.com>
2021-01-28 12:52:12 +0300

    Initailise admin item tests

---
+++ tests/test_admin_item.py
@@ -0,0 +1,54 @@
+import unittest
+import json
+from application import create_app, db
+import io
+
+class AuthTestCase(unittest.TestCase):
+    """Test case for the admin blueprint"""
+
+    def setUp(self):
+        """Set up test variables"""
+        self.app = create_app(config_name="testing")
+        # initialize the test client
+        self.client = self.app.test_client
+
+        self.user_data = {
+            'username': "Admin",
+            'email': 'test@example.com',
+            'password': 'test_password'
+        }
+        self.dataset = {'name': 'Cervical Infection', 'classes': ["Positive", "Negative", "Not Sure"]}
+        self.item = {
+            
+        }
+
+        # Binds application to current context
+        with self.app.app_context():
+            #create all tables
+            db.create_all()
+
+        #===================================================================
+    # def test_item_upload(self):
+    #     """Test if API can add item to dataset"""
+    #     #Upload dataset
+    #     dataset_res = self.client().post('/admin/dataset/', data=self.dataset)
+    #     self.assertEqual(dataset_res.status_code, 201)
+    #     dataset_json = json.loads(dataset_res.data.decode())
+
+    #     #Upload item
+
+    #     item_res = self.client().post('/admin/datasets/item/', data=self.item, dataset_id=dataset_json['id'])
+    #     self.assertEqual(item_res.status_code, 201)
+    #     self.assertIn("Item1", str(item_res.data))
+
+
+
+    def tearDown(self):
+        """teardown all initialized variables"""
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+
+if __name__ == "__main__":
+    unittest.main()
