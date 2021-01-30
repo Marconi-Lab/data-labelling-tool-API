c670f153d119f5fca0cf2d224f4da9ca6f298456
ben-wycliff <ben12wycliff@gmail>
2021-01-30 15:46:59 +0300

    [BUG-FIX] User blueprint name

--- application/user/__init__.py
+++ application/user/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint, request, jsonify, abort
 from application.models import Image, Item, User, Assignment, Dataset
 
-admin_blueprint = Blueprint('user', __name__)
+user_blueprint = Blueprint('user', __name__)
