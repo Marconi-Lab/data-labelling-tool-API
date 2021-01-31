e2483f87d03cc7aec7b15baae8a8ec3f79b0d6bf
ben-wycliff <ben12wycliff@gmail.com>
2021-01-31 15:37:03 +0300

    Creates user token verification decorator

--- application/decorators.py
+++ application/decorators.py
@@ -22,11 +22,22 @@ def permission_required():
             return f(*args, **kwargs)
         return __permission_required
     return _permission_required
-        # def decorator(f):
-        #     @wraps(f)
-        #     def decorated_function(*args, **kwargs):
-        #         if not current_user.can(permission):
-        #             abort(403)
-        #         return f(*args, **kwargs)
-        #     return decorated_function
-        # return decorator
+
+def user_is_authenticated():
+    def _user_is_authenticated(f):
+        @wraps(f)
+        def __user_is_authenticated(*args, **kwargs):
+            auth_headers = request.headers.get("Authorization")
+            if not auth_headers:
+                abort(401)
+            access_token = auth_headers.split(" ")[1]
+            if access_token:
+                try:
+                    user_id = User.decode_token(access_token)
+                    if not str(user_id) == str(request.headers.get("user_id")):
+                        abort(401, "Not authorized" + user_id + "!= "+request.headers.get("user_id"))
+                except Exception as e:
+                    abort(401, e)
+            return f(*args, **kwargs)
+        return __user_is_authenticated
+    return _user_is_authenticated
