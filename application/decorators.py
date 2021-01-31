c6149d154330d8a2ffd964382616aec8de1b411b
ben-wycliff <ben12wycliff@gmail.com>
2021-01-31 12:18:42 +0300

    Creates authorization decorator

--- application/decorators.py
+++ application/decorators.py
@@ -1,15 +1,20 @@
-def permission_required(function):
-    def wrap(self, request, *args, **kwargs):
-        auth_headers = request.headers.get("Authorization")
-        access_token = auth_headers.split(" ")[1]
-        if not access_token:
-            abort(401)
-        if not request.headers.get("is_admin"):
-            abort(403)
-        return function(self, request, *args, **kwargs)
-    wrap.__doc__ = function.__doc__
-    wrap.__name__ = function.__name__
-    return wrap
+from functools import wraps
+from flask import request, abort
+
+def permission_required():
+    def _permission_required(f):
+        @wraps(f)
+        def __permission_required(*args, **kwargs):
+            auth_headers = request.headers.get("Authorization")
+            if not auth_headers:
+                abort(401)
+            access_token = auth_headers.split(" ")[1]
+          
+            if not request.headers.get("is_admin"):
+                abort(403)
+            return f(*args, **kwargs)
+        return __permission_required
+    return _permission_required
         # def decorator(f):
         #     @wraps(f)
         #     def decorated_function(*args, **kwargs):
