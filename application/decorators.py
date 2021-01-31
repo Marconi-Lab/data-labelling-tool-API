48e7e237a93175bed3557587cc0afcda0dbea019
ben-wycliff <ben12wycliff@gmail.com>
2021-01-31 15:29:44 +0300

    Verifies token belongs to current admin

--- application/decorators.py
+++ application/decorators.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from flask import request, abort
+from application.models import User
 
 def permission_required():
     def _permission_required(f):
@@ -9,7 +10,13 @@ def permission_required():
             if not auth_headers:
                 abort(401)
             access_token = auth_headers.split(" ")[1]
-          
+            if access_token:
+                try:
+                    user_id = User.decode_token(access_token)
+                    if not str(user_id) == str(request.headers.get("user_id")):
+                        abort(401, "Not authorized" + user_id + "!= "+request.headers.get("user_id"))
+                except Exception as e:
+                    abort(401, e)
             if not request.headers.get("is_admin"):
                 abort(403)
             return f(*args, **kwargs)
