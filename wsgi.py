18fa95be7ac6c1ade7322e8268cb4a71ec110adf
ben-wycliff <ben12wycliff@gmail.com>
2021-02-18 11:44:17 +0300

    Creates wsgi file to run gunicorn server

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+import os
+from flask import logging
+
+from application import create_app
+
+config_name = os.getenv("APP_SETTINGS")
+app = create_app(config_name)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+if __name__ == "__main__":
+    app.run()
