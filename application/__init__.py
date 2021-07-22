2f12af947e1a807fe19e6e728e6a2d83b3b987cf
ben-wycliff <ben12wycliff@gmail.com>
2021-07-22 10:48:26 +0300

    Adds orchestra uploads endpoint

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_api import FlaskAPI
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from instance.config import app_config
-
+import os
 load_dotenv()
 
 db = SQLAlchemy()
@@ -19,15 +19,19 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['EXTERNAL_UPLOADS'] = os.path.join(os.path.dirname(__file__), os.environ.get("UPLOAD_FOLDER"), "external" )
     # user_manager = UserManager(app, db, User)
-    db.init_app(app)
+    with app.app_context():
+        db.init_app(app)
+        from .auth import auth_blueprint
+        app.register_blueprint(auth_blueprint, url_prefix="/api/v1")
 
-    from .auth import auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix="/api/v1")
+        from .admin import admin_blueprint
+        app.register_blueprint(admin_blueprint, url_prefix="/api/v1")
 
-    from .admin import admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix="/api/v1")
+        from .user import user_blueprint
+        app.register_blueprint(user_blueprint, url_prefix="/api/v1")
 
-    from .user import user_blueprint
-    app.register_blueprint(user_blueprint, url_prefix="/api/v1")
+        from .external import external_blueprint
+        app.register_blueprint(external_blueprint, url_prefix="/api/v1")
     return app
--- application/external/__init__.py
+++ application/external/__init__.py
@@ -1,11 +1,110 @@
 import os
+import requests
 from PIL import Image as Img
+import uuid
 
-from flask import request,  Blueprint, jsonify, url_for
+from flask import request, Blueprint, jsonify, url_for
 from werkzeug.utils import secure_filename
 
-import application as app
-from application.models import Image, Item, User, Assignment, Dataset
+from flask import current_app as app
+from application.models import Image, Item, Dataset
 from ..admin import allowed_file
 
 external_blueprint = Blueprint("external", __name__)
+
+uploads_dir = app.config["EXTERNAL_UPLOADS"]
+resized_uploads_dir = os.path.join(app.config["EXTERNAL_UPLOADS"], "resized")
+
+
+def create_image(picture, folder_name, folder_id, dataset_id, image_label):
+    image = Img.open(requests.get(picture, stream=True).raw)
+    if allowed_file(image.get_format_mimetype()):
+        print(image.get_format_mimetype(), "  image name ", image.filename)
+        image_name = secure_filename(folder_name +"-"+ str(uuid.uuid4())+ ".jpg")
+        print("Image name ", image_name)
+        image_resized = image.resize((512, 512), Img.ANTIALIAS)
+        # save original
+        image.save(os.path.join(uploads_dir, image_name))
+        print("saved image")
+        # save resized
+        image_resized.save(os.path.join(resized_uploads_dir, image_name))
+
+        image_url = url_for(os.environ.get("UPLOAD_FOLDER"), filename=f"external/resized/{image_name}", _external=True)
+        print("image_url: ", image_url)
+        image_upload = Image(name=image_name, image_URL=image_url)
+        print("Created image upload")
+        image_upload.item_id = folder_id
+        image_upload.dataset_id = dataset_id
+        image_upload.label = image_label
+        image_upload.labelled = True
+        image_upload.save()
+        print("Concluded image upload")
+    return picture
+
+
+@external_blueprint.route("/upload", methods=["POST"])
+def upload_data():
+    try:
+        payload = request.get_json()
+        print("Payload ", payload["picture1_before"])
+        #  check if dataset exists if not create it
+        datasets = Dataset.query.filter_by(name="orchestra_data").all()
+        print("datasets ", datasets)
+        if len(datasets) > 0:
+            dataset = datasets[0]
+        else:
+            dataset = Dataset(name="orchestra_data",
+                              classes=["VIA Positive", "VIA Negative", "Suspicious of cancer", "Other lesions",
+                                       "Bad quality images"])
+            dataset.save()
+        #  create item
+        folder = Item(name=payload["study_id"], dataset_id=dataset.id)
+        folder.save()
+        #  create image_1
+        image1_label = "Stained with acetic acid" if payload["picture1_before"]["acetic_acid"] else "Not stained with acetic acid"
+        print("Image 1 label: ", image1_label)
+        image1_url = create_image(payload["picture1_before"]["request_image_url"], folder.name, folder.id, dataset.id, image1_label)
+
+        #  create image_2
+        image2_label = "Stained with acetic acid" if payload["picture2_before"]["acetic_acid"] else "Not stained with acetic acid"
+        image2_url = create_image(payload["picture2_before"]["request_image_url"], folder.name, folder.id, dataset.id, image2_label)
+
+        #  create image_3
+        image3_label = "Stained with acetic acid" if payload["picture3_after"]["acetic_acid"] else "Not stained with acetic acid"
+        image3_url = create_image(payload["picture3_after"]["request_image_url"], folder.name, folder.id, dataset.id, image3_label)
+
+        #  create image_4
+        image4_label = "Stained with acetic acid" if payload["picture4_after"]["acetic_acid"] else "Not stained with acetic acid"
+        image4_url = create_image(payload["picture4_after"]["request_image_url"], folder.name, folder.id, dataset.id, image4_label)
+
+        response = jsonify({
+            "picture1_before": {
+                "request_image_url": image1_url,
+                "pred_class": "",
+                "negative_confidence": "0",
+                "positive_confidence": "0"
+            },
+            "picture2_before": {
+                "request_image_url": image2_url,
+                "pred_class": "",
+                "negative_confidence": "0",
+                "positive_confidence": "0"
+            },"picture3_before": {
+                "request_image_url": image3_url,
+                "pred_class": "",
+                "negative_confidence": "0",
+                "positive_confidence": "0"
+            },"picture4_before": {
+                "request_image_url": image4_url,
+                "pred_class": "",
+                "negative_confidence": "0",
+                "positive_confidence": "0"
+            },
+        })
+        return response
+    except Exception as msg:
+        response = jsonify({
+            "message": f"Error: {msg}"
+        })
+        print(msg)
+        # return response
