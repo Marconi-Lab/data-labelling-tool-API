8bb3dc02caf968c9ea41f1109c1994fdbe8e340a
ben-wycliff <ben12wycliff@gmail.com>
2021-07-23 14:26:43 +0300

    Reconfigures api dashboard settings

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,7 @@ from flask_api import FlaskAPI
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from instance.config import app_config
+import flask_monitoringdashboard as dashboard
 import os
 load_dotenv()
 
@@ -20,7 +21,8 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['EXTERNAL_UPLOADS'] = os.path.join(os.path.dirname(__file__), os.environ.get("UPLOAD_FOLDER"), "external" )
-    # user_manager = UserManager(app, db, User)
+    dashboard.config.init_from(file="../dashboard_config.cfg")
+    dashboard.bind(app)
     with app.app_context():
         db.init_app(app)
         from .auth import auth_blueprint
