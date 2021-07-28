ba42669a2d81e8d80a57a0fea91b821959b8dbfa
ben-wycliff <ben12wycliff@gmail.com>
2021-07-28 11:15:43 +0300

    Ensures correct image orientation

--- application/external/predict.py
+++ application/external/predict.py
@@ -43,3 +43,41 @@ def initialize():
         global labels
         labels = [l.strip() for l in lf.readlines()]
     print(len(labels), "found. Success!")
+
+
+def log_msg(msg):
+    print("{}: {}".format(datetime.now(), msg))
+
+
+def update_orientation(image):
+    """
+    corrects image orientation according to EXIF data
+    image: input PIL image
+    returns corrected PIL image
+    """
+    exif_orientation_tag = 0x0112
+    if hasattr(image, "_getexif"):
+        exif = image._getexif()
+        if exif != None and exif_orientation_tag in exif:
+            orientation = exif.get(exif_orientation_tag, 1)
+            log_msg("Image has EXIF Orientation: " + str(orientation))
+            # orientation is 1 based, shift to zero based and flip/transpose based on 0-based values
+            orientation -= 1
+            if orientation >= 4:
+                image = image.transpose(Image.TRANSPOSE)
+            if (
+                orientation == 2
+                or orientation == 3
+                or orientation == 6
+                or orientation == 7
+            ):
+                image = image.transpose(Image.FLIP_TOP_BOTTOM)
+            if (
+                orientation == 1
+                or orientation == 2
+                or orientation == 5
+                or orientation == 6
+            ):
+                image = image.transpose(Image.FLIP_LEFT_RIGHT)
+    return image
+
