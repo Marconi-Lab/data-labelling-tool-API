c2a60aacf6e6f7c43e269009a2dbce1205faf829
ben-wycliff <ben12wycliff@gmail.com>
2022-01-16 18:46:21 +0300

    [BUG-FIX] corrects key name

--- application/auth/__init__.py
+++ application/auth/__init__.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect
 
 from application.utils.token import confirm_verification_token
 
 from application.models import User
+
+import os
 
 #Authentication blueprint instance
 auth_blueprint = Blueprint('auth', __name__)
@@ -14,21 +16,18 @@ def verify_email(token):
     try:
         email = confirm_verification_token(token)
 
-        user = User.query.filter_by(email=email).fist()
-        if user.isVerified:
+        user = User.query.filter_by(email=email).first()
+        if user.is_verified:
             response = jsonify({
                 "message": "Invalid input"
             })
             response.status_code = 422
             return response
         else:
-            user.isVerified = True
+            user.is_verified = True
             user.save()
-            response = jsonify({
-                'message': 'E-mail verified, you can proceed to login now.'
-            })
-            response.status_code = 200
-            return response
+            
+            return redirect(os.getenv("FRONT_END_LOGIN"), code=302)
     except Exception as e:
         print(e)
         return e
